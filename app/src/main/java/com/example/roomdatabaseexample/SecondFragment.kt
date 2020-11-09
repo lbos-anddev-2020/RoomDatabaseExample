8ce062e66fd0abb8327856cff38ba400ad5b66de
lbos-anddev-2020 <github2020@backhaus-os.de>
2020-11-09 11:43:38 +0100

    set Navigation1

--- app/src/main/java/com/example/roomdatabaseexample/SecondFragment.kt
+++ app/src/main/java/com/example/roomdatabaseexample/SecondFragment.kt
@@ -6,7 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.FragmentNavigator
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -24,6 +29,7 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        button2.setOnClickListener { findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment2)}
 
     }
 }
